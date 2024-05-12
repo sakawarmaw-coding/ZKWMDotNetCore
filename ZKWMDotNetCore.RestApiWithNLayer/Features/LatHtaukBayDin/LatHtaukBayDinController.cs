2f0354c69adbd4f499ac00f8c7a421a57f829ad5
Milo Visibee <Milo Visibee@DESKTOP-6QTP69L>
2024-05-12 09:13:53 +0630

    Add LatHtaukBayDin

---
+++ ZKWMDotNetCore.RestApiWithNLayer/Features/LatHtaukBayDin/LatHtaukBayDinController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+using ZKWMDotNetCore.RestApiWithNLayer.Features.Blog;
+
+namespace ZKWMDotNetCore.RestApiWithNLayer.Features.LatHtaukBayDin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LatHtaukBayDinController : ControllerBase
+    {
+        private async Task<LatHtaukBayDin> GetDataAsync()
+        {
+            var jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
+            var model = JsonConvert.DeserializeObject<LatHtaukBayDin>(jsonStr);
+            return model;
+        }
+
+        [HttpGet("questions")]
+        public async Task<IActionResult> Questions()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.questions);
+        }
+
+        [HttpGet("numberlist")]
+        public async Task<IActionResult> NumberList()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.numberList);
+        }
+
+        [HttpGet("{questionNo}/{no}")]
+        public async Task<IActionResult> Answers(int questionNo,int no)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.answers.FirstOrDefault(x=>x.questionNo==questionNo && x.answerNo==no));
+        }
+
+        public class LatHtaukBayDin
+        {
+            public Question[] questions { get; set; }
+            public Answer[] answers { get; set; }
+            public string[] numberList { get; set; }
+        }
+
+        public class Question
+        {
+            public int questionNo { get; set; }
+            public string questionName { get; set; }
+        }
+
+        public class Answer
+        {
+            public int questionNo { get; set; }
+            public int answerNo { get; set; }
+            public string answerResult { get; set; }
+        }
+    }
+}
