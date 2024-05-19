94fac87ac8201a96c191186021fd95101c324f85
Milo Visibee <Milo Visibee@DESKTOP-6QTP69L>
2024-05-19 11:49:32 +0630

    [Complete] Pizza

---
+++ ZKWMDotNetCore.PizzaApi/Db/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZKWMDotNetCore.PizzaApi.Db
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString.sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet<PizzaModel> Pizzas { get; set; }
+        public DbSet<PizzaExtraModel> PizzaExtras { get; set; } 
+    }
+}
+
+[Table("Tbl_Pizza")]
+public class PizzaModel
+{
+    [Key]
+    [Column("PizzaId")]
+    public int Id { get; set; }
+    [Column("Pizza")]
+    public string Name { get; set; }
+    [Column("Price")]
+    public decimal Price { get; set; }
+}
+
+[Table("Tbl_PizzaExra")]
+public class PizzaExtaModel
+{
+    [Key]
+    [Column("PizzaExtraId")]
+    public int Id { get; set; }
+    [Column("PizzaExtraName")]
+    public string Name { get; set; }
+    [Column("Price")]
+    public decimal Price { get; set; }
+    [NotMapped]
+    public string PriceStr { get { return "$ " + Price; } }
+}
+
+public class OrderRequest
+{
+    public int PizzaId { get; set; }
+    public int[] Extras { get; set; }
+}
---
+++ ZKWMDotNetCore.PizzaApi/Features/Pizza/PizzaController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZKWMDotNetCore.PizzaApi.Db;
+
+namespace ZKWMDotNetCore.PizzaApi.Features.Pizza
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PizzaController()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var lst = await _appDbContext.Pizzas.ToListAsync();
+            return Ok(lst);
+        }
+
+        [HttpGet("Extras")]
+        public async Task<IActionResult> GetExtra()
+        {
+            var lst = await _appDbContext.Pizzas.ToListAsync();
+            return Ok(lst);
+        }
+
+        [HttpPost("Order")]
+        public async Task<IActionResult> Order(OrderRequest orderRequest)
+        {
+            return Ok(orderRequest);
+        }
+    }
+}
