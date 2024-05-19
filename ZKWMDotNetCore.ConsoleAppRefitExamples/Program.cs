860ccb2f83a2b0670a497894129085cb3e63bf14
Milo Visibee <Milo Visibee@DESKTOP-6QTP69L>
2024-05-19 17:46:57 +0630

    [Complete] Refit CRUD

---
+++ ZKWMDotNetCore.ConsoleAppRefitExamples/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Refit;
+using ZKWMDotNetCore.ConsoleAppRefitExamples;
+
+//try
+//{
+//    RefitExample refitExample = new RefitExample();
+//    await refitExample.RunAsync();
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.ToString());
+//}
+
+RefitExample refitExample = new RefitExample();
+await refitExample.RunAsync();
