81dca9e88f88210577c3bb355f30d94e7af54dd6
SakawarMaw <tzakawarmaung@gmail.com>
2024-07-07 11:39:01 +0630

    [Complete] ASP.NET Core MVC Ajax

---
+++ ZKWMDotNetCore.MvcApp2/Db/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZKWMDotNetCore.MvcApp2.Models;
+
+namespace ZKWMDotNetCore.MvcApp2.Db;
+
+public class AppDbContext : DbContext
+{
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlServer(ConnectionString.sqlConnectionStringBuilder.ConnectionString);
+    //}
+
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ ZKWMDotNetCore.MvcApp2/Models/BlogModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZKWMDotNetCore.MvcApp2.Models;
+
+[Table("Tbl_Blog")]
+public class BlogModel
+{
+    [Key]
+    public int BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+}
+
+public class MessageModel
+{
+    public bool IsSuccess { get; set;}
+    public string Message { get; set; }
+}
