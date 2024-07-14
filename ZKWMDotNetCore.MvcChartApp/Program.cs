36f10eb452dd054d85a7ec13c9388aff59c6c163
SakawarMaw <tzakawarmaung@gmail.com>
2024-07-14 18:43:45 +0630

    [Complete] DbLog

--- ZKWMDotNetCore.MvcChartApp/Program.cs
+++ ZKWMDotNetCore.MvcChartApp/Program.cs
@@ -1,27 +1,59 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs/DotNetTrainingBatch4.MvcChartApp.log");
 
-var app = builder.Build();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(filePath, rollingInterval: RollingInterval.Hour)
+    .WriteTo
+    .MSSqlServer(
+        connectionString: "Server=ZAKAWARMAW\\SQL2022;Database=DotNetTrainingBatch4;User ID=sa;Password=sa@123;TrustServerCertificate=True;",
+        sinkOptions: new MSSqlServerSinkOptions
+        {
+            TableName = "Tbl_LogEvents",
+            AutoCreateSqlTable = true
+        })
+    .CreateLogger();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddSerilog(); // <-- Add this line
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
