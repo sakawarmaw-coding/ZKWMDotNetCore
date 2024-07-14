8872f208f4f4b221e89121384baed0d703df5d52
SakawarMaw <tzakawarmaung@gmail.com>
2024-07-14 18:17:47 +0630

    [Complete] High Chart

---
+++ ZKWMDotNetCore.MvcChartApp/Controllers/HighChartsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZKWMDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartsController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
