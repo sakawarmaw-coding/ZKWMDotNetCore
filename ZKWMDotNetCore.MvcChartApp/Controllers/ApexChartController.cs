1050bacf63f56a4d55edceb383798add691bf1b4
SakawarMaw <tzakawarmaung@gmail.com>
2024-07-14 18:03:54 +0630

    [Complete]Apex Chart And ChartJs

---
+++ ZKWMDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZKWMDotNetCore.MvcChartApp.Models;
+
+namespace ZKWMDotNetCore.MvcChartApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult SimplePieChart()
+        {
+            PieChartModel model = new PieChartModel();
+            model.Lables = new List<string>() { "Team A", "Team B", "Team C", "Team D", "Team E" };
+            model.Series = new List<int> { 44, 55, 13, 43, 22 };
+
+            return View(model);
+        }
+    }
+}
---
+++ ZKWMDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZKWMDotNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+
+        public IActionResult ExampleChart()
+        {
+            return View();
+        }
+
+        public IActionResult InterpolationLineChart()
+        {
+            return View();
+        }
+
+    }
+}
