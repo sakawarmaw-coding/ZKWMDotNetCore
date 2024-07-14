6ab34f824758fa704c99ab169d8b0c72fff42656
SakawarMaw <tzakawarmaung@gmail.com>
2024-07-14 22:54:43 +0630

    [Complete] Realtime Chat Using SignalR

---
+++ ZKWMDotNetCore.RealtimeChartApp/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ZKWMDotNetCore.RealtimeChartApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //Send Message
+        public async Task ServerReceiveMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ClientReceiveMessage", user, message);
+        }
+    }
+}
